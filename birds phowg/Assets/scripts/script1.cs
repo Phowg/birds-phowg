f2507fd18686bef4f88d86da655b32a61d2716a7
phowg <phoowg@gmail.com>
2021-01-20 14:41:51 +0100

    20/01/2021 primer commi flappy bird

---
+++ birds phowg/Assets/scripts/script1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    private int posicionx;
+
+    
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        posicionx = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = 
+    }
+
+}
